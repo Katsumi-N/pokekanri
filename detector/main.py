14898959757afd06c315c83d3d2f5e0ca77e12c8
Katsumi-N <naya.katsumi@gmail.com>
2024-09-19 09:45:31 +0900

    use easyocr

--- detector/main.py
+++ detector/main.py
@@ -1,29 +1,25 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
-from PIL import Image, ImageDraw
+from PIL import Image
 import io
 import tempfile
 import easyocr
 
 app = FastAPI()
 
-UPLOAD_DIR = "images"
-
 @app.post("/image")
-async def upload_image(file: UploadFile = File(...)):
+async def upload_image(file: UploadFile = File(...), confident: float = 0.5):
     reader = easyocr.Reader(['en'], gpu=False)
     image = Image.open(io.BytesIO(await file.read()))
     with tempfile.NamedTemporaryFile(suffix=".png", delete=True) as tmp:
         image.save(tmp.name)
         result = reader.readtext(tmp.name)
-        
-    card_ids = []
+
+    card_text = []
     for res in result:
-        (_, txt, _) = res
+        (_, txt, conf) = res
         
-        if '/' in txt:
-            splited_txt = txt.split(' ')
-            for s in splited_txt:
-                card_ids.append(s)
-    
-    return JSONResponse(content={"ocr_result": card_ids})
+        if conf > confident:
+            card_text.append(txt)
+
+    return JSONResponse(content={"ocr": card_text})
