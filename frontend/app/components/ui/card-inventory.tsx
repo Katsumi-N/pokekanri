7c1b0acaaf943542daa8e56a68a210b74bd6f9b3
Katsumi-N <naya.katsumi@gmail.com>
2024-11-29 08:38:14 +0900

    add react-icons

--- frontend/app/components/ui/card-inventory.tsx
+++ frontend/app/components/ui/card-inventory.tsx
@@ -5,6 +5,7 @@ import { useCardContext } from '@/context/CardContext';
 import { Button } from "@/components/ui/shadcn/button";
 import Image from 'next/image';
 import { CardInfo } from '@/lib/card';
+import { FaTrash, FaSave } from 'react-icons/fa';
 
 export default function CardInventory() {
   const { cards, deleteCard, saveCardQuantity } = useCardContext();
@@ -22,17 +23,26 @@ export default function CardInventory() {
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
         {cards.map((card) => (
-          <div key={card.id} className="bg-white shadow-md rounded-lg p-4">
+          <div key={card.id} className="bg-white shadow-md rounded-lg p-4 flex flex-col">
             <Image src={card.image_url} alt={card.name} width={64} height={64} className="object-cover rounded-md mr-4" />
             <h3 className="text-lg font-semibold">{card.name}</h3>
             <p className="text-sm text-gray-600">{card.type} {card.hp ? `- HP ${card.hp}` : ""}</p>
-            <p className="text-sm text-gray-600">枚数: {card.quantity}</p>
+            <p className="text-sm text-gray-600">枚数: {card.quantity || 0}</p>
             <div className="flex items-center space-x-2 mt-2">
               <Button onClick={() => decrementCard(card.id)} className="px-2 py-1">-</Button>
-              <span className="text-sm text-gray-600">{tempQuantities[card.id]}</span>
+              <span className="text-sm text-gray-600">{tempQuantities[card.id] || 0}</span>
               <Button onClick={() => incrementCard(card)} className="px-2 py-1">+</Button>
-              <Button onClick={() => deleteCard(card.id)} className="ml-auto px-2 py-1 bg-red-500 text-white">Delete</Button>
-              <Button onClick={() => saveCardQuantity(card, tempQuantities[card.id])} className="ml-auto px-2 py-1 bg-green-500 text-white">Save</Button>
+
+              <Button onClick={() => saveCardQuantity(card, tempQuantities[card.id])} className="px-2 py-1 bg-green-500">
+                <FaSave />
+              </Button>
+              <Button onClick={() => deleteCard(card.id)} className="px-2 py-1 bg-red-500">
+                <FaTrash />
+              </Button>
+
+            </div>
+            <div className="flex items-center space-x-2 mt-2">
+
             </div>
           </div>
           
