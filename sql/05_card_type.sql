7cbf3554beecdf971b1073b74686ae0fc35d140f
Katsumi-N <naya.katsumi@gmail.com>
2025-01-04 23:26:01 +0900

    schema change

---
+++ sql/05_card_type.sql
@@ -0,0 +1,4 @@
+USE `pokekanridb`;
+INSERT INTO card_types (id, name) VALUES (1, 'ポケモン');
+INSERT INTO card_types (id, name) VALUES (2, 'トレーナーズ');
+INSERT INTO card_types (id, name) VALUES (3, 'エネルギー');
